e469008e693151e322d53e6b0497ad110dca3b70
ildevelop <raduilya@gmail.com>
2018-08-04 15:22:32 +0300

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Container } from 'reactstrap';
+import * as selector from './../selectors'
+import * as mainActions from '../actions/mainActions';
+import Header from "../components/Header/Header";
+import BookList from "../components/BookList/BookList";
+import MyBook from "../components/MyBook/MyBook";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      itemsPerPage: 20
+    };
+  };
+
+  componentDidMount() {
+    this.props.getBook();
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("Books", JSON.stringify(this.props.Books));
+  }
+  handleSearchBook = value => {
+    this.props.searchBook(value);
+  };
+  handleRemoveMyBook = Book => {
+    this.props.removeBook(Book);
+  };
+  handleAddBook= Book => {
+    this.props.addBook(Book);
+  };
+
+  page = page => {
+    this.setState({
+      currentPage: page
+    });
+  };
+
+  render() {
+    const { loaded,searchValue,searchedUsers,myBooks} = this.props;
+    const {itemsPerPage, currentPage} = this.state;
+
+    return (
+      <Container>
+        <Header />
+        <Switch>
+          <Route
+            exact path="/"
+            render={() => {
+              return (
+                <BookList
+                  loaded={loaded}
+                  Books={searchedUsers}
+                  value={searchValue}
+                  onInputChange={this.handleSearchBook}
+                  onAddBook={this.handleAddBook}
+                  onRemoveBook={this.handleRemoveMyBook}
+                  currentPage={currentPage}
+                  itemsPerPage={itemsPerPage}
+                  page={this.page}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/myBooks"
+            render={() => {
+              return (
+                <MyBook
+                  loaded={loaded}
+                  Books={myBooks}
+                  onRemovemyBook={this.handleRemoveMyBook}
+                  onAddToFavemyBook={this.handleAddBook}
+                />
+              );
+            }}
+          />
+
+        </Switch>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loaded: selector.getLoadingStatus(state),
+  Book: selector.getBook(state),
+  Books: selector.getBooks(state),
+  searchValue:selector.getSearchValue(state),
+  searchedUsers: selector.getSearchedBook(state),
+  myBooks: selector.getMyBooks(state),
+
+
+});
+
+function mapDispatchToProps(dispatch) {
+  return {...bindActionCreators(mainActions, dispatch)}
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
---
+++ src/selectors.js
@@ -0,0 +1,15 @@
+import { createSelector } from 'reselect';
+
+export const getBooks = state => state.Books;
+export const getBook = state => state.Book;
+export const getSearchValue = state => state.searchedValue;
+export const getLoadingStatus = state => state.loaded;
+
+export const getSearchedBook= createSelector(getBooks, getSearchValue, (Books, searchValue) => {
+  return Books.filter(Book => Book.title.includes(searchValue) || Book.author.includes(searchValue));
+});
+
+export const getMyBooks = createSelector(getBooks, (Books) => {
+  return Books.filter(Book => Book.isBook);
+});
+
