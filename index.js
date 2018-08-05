b460031213018ab138a42f2f606d95b7080a0202
ildevelop <raduilya@gmail.com>
2018-08-05 20:04:58 +0300

    minor code fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .set('/static', path.join(__dirname, 'build/static'))
+  .get('/', (req, res) => res.render('build/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
