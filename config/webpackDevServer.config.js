c5dba63842ee065a333beee259434876bf88af40
ildevelop <raduilya@gmail.com>
2018-08-05 18:33:06 +0300

    deploy to heroku

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -7,7 +7,7 @@ const config = require('./webpack.config.dev');
 const paths = require('./paths');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || 'https://herolo-book.herokuapp.com/';
 
 module.exports = function(proxy, allowedHost) {
   return {
